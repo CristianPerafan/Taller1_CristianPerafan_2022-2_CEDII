f6281e9f02d9594d4f4bfb7b4d186c4cd1599c56
CristianPerafan <1110282139@u.icesi.edu.co>
2022-08-13 22:15:41 -0500

    Exercise 4,5,6 and 8 were done

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,9 +2,9 @@ object Main {
   def main(args: Array[String]): Unit = {
     println("Recursive Functions")
 
-    def suc(n : Int): Int = n+1
-    def pred(n : Int): Int = n-1
+    def suc(n: Int): Int = n + 1
 
+    def pred(n: Int): Int = n - 1
 
     /*
     Exercise 1
@@ -23,10 +23,49 @@ object Main {
     /*
     Exercise 3
       */
-    def sum(n: Int): Int = {
-      if (n == 0) n else n + sum(pred(n))
+    def rSum(n: Int): Int = {
+      if (n == 0) n else n + rSum(pred(n))
     }
 
+    /*
+    Exercise 4
+      */
+    def suma(x : Int, y : Int): Int = {
+      if(x == 0) y else suc(suma(pred(x),y))
+    }
+
+    /*
+    Exercise 5
+      */
+    def dif(x: Int, y : Int): Int = {
+      if(y == 0) x else dif(x,pred(y))-1
+    }
+
+    /*
+    Exercise 6
+      */
+
+    def prod(x: Int, y: Int): Int = {
+      if(x == 0) 0 else{
+        if(x == 1) 1 else {
+          suma(y,prod(pred(x),y))
+        }
+      }
+    }
+
+    /*
+    Exercise 7
+      */
+    def exercise(x: Int, y : Int): Double = {
+      1
+    }
+    /*
+    Exercise 8
+      */
+
+    def modulo(m : Int, n : Int): Int = {
+      if(m<n) m else modulo(dif(m,n),n)
+    }
 
   }
 
